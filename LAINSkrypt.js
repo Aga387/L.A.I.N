6baaa937a8b54d40b1b94f46859622d2ed45c93d
Aga387 <agakotula94@gmail.com>
2026-05-24 13:37:53 +0200

    L.A.I.N 1.0

---
+++ LAINSkrypt.js
@@ -0,0 +1,67 @@
+const hasloTekst = document.getElementById('haslo');
+const odpowiedz = document.getElementById('odpowiedz');
+
+hasloTekst.addEventListener('input', function(){
+    const haslo = hasloTekst.value.toLowerCase().trim();
+
+    if (haslo === "hej"||haslo === "czesc"){
+        odpowiedz.textContent = "hej..."
+    }
+    else if (haslo === "edith muller"){
+        odpowiedz.textContent = "jak dlugo jeszcze chcesz to ciagnac...?"
+        }
+    else if (haslo === "edmund james kelly"||haslo === "eddie"){
+        odpowiedz.textContent = "tez chcesz byc tacy jak oni...?"
+    }
+    else if (haslo === "helga schneider"){
+        odpowiedz.textContent = ""
+    }
+    else if  (haslo === "mika stein"||haslo === "mika"||haslo === "agnes hoffman"||haslo === "agnes hoffman schulz"){
+        odpowiedz.textContent = "biedne dziecko... sed nunc bene est..."
+    }
+    else if (haslo === "alina sorge"){
+        odpowiedz.textContent = "jestes z siebie dumna...?"
+    }
+    else if (haslo === "liliana sorge"){
+        odpowiedz.textContent = "boli...? dobrze..."
+    }
+    else if (haslo === "johann kruger"){
+        odpowiedz.textContent = "dzieki tobie w koncu beda szczesliwi... pospiesz sie..."
+    }
+    else if (haslo === "franz stein"){
+        odpowiedz.textContent = "az mi cie czasem zal... stultus..."
+    }
+    else if (haslo === "rachel vogel"){
+        odpowiedz.textContent = "wybacz mi... ze cie okradli..."
+    }
+    else if (haslo === "martha lindemann"){
+        odpowiedz.textContent = "nie martw sie... juz jestes bezpieczna... pauper hedum..."
+    }
+    else if (haslo === "wilhelm wegscheider"||haslo === "ronald wegscheider"){
+        odpowiedz.textContent = "nie idz tam... oni klamia... to nie jest tego warte..."
+    }
+    else if (haslo === "policja"){
+        odpowiedz.textContent = "miseret me tibi... sed non mihi est eligere... habet ut stipendium..."
+    }
+    else if (haslo === "naukowcy"){
+        odpowiedz.textContent = "fruere... mors veniet... sive vis sive non..."
+    }
+    else if (haslo === "narkotyki"){
+        odpowiedz.textContent = "Putasne vere id adiuturum esse...? adhuc eos dolet..."
+    }
+    else if (haslo === "serwerownia"){
+        odpowiedz.textContent = "mitescere nunc... scio dolere..."
+    }
+    else if (haslo === "las"){
+        odpowiedz.textContent = "arbor inversa in silva est... et in ligno... solucionis..."
+    }
+    else if (haslo === "ewikgeisstadt"){
+        odpowiedz.textContent = "miasto wiecznosci...? co za ironia..."
+    }
+    else if (haslo === "lain"||haslo === "l.a.i.n"){
+        odpowiedz.textContent = "to... ja...?"
+    }
+    else{
+        odpowiedz.textContent = "";
+    }
+});
